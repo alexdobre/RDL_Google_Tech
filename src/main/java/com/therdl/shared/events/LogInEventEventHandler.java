958e86c30cf41bddbe9381c59925c03d1234bc8b
n <n@n-desktop.(none)>
2013-10-19 18:59:31 +0300

    documentation

--- src/main/java/com/therdl/shared/events/LogInEventEventHandler.java
+++ src/main/java/com/therdl/shared/events/LogInEventEventHandler.java
@@ -3,6 +3,10 @@ package com.therdl.shared.events;
 
 import com.google.gwt.event.shared.EventHandler;
 
+/**
+ * Simple event handler for the  log-in event handler no payload required
+ */
+
 public interface LogInEventEventHandler  extends EventHandler {
 
     void onLogInEvent(LogInEvent onLogInEvent);
