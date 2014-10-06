6ddd043071ad2044d240d1d5f45194ee8cacd122
alexdobre <alx.dobre@gmail.com>
2014-10-06 18:42:11 +0100

    the original runt and func :)

---
+++ src/main/java/com/therdl/client/presenter/func/TheOriginalFunc.java
@@ -0,0 +1,7 @@
+package com.therdl.client.presenter.func;
+
+/**
+ * Created by Alex on 10/6/14.
+ */
+public class TheOriginalFunc {
+}
---
+++ src/main/java/com/therdl/client/presenter/runt/TheOriginalRunt.java
@@ -0,0 +1,7 @@
+package com.therdl.client.presenter.runt;
+
+/**
+ * Created by Alex on 10/6/14.
+ */
+public class TheOriginalRunt {
+}
