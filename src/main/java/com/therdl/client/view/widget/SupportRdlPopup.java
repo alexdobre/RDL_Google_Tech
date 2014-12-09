febe1d8752b60f8fbdc95fe52234123a9d7b7f9d
markdiesta <markdiesta@gmail.com>
2014-12-09 08:05:53 +0800

    paypal currency configuration

--- src/main/java/com/therdl/client/view/widget/SupportRdlPopup.java
+++ src/main/java/com/therdl/client/view/widget/SupportRdlPopup.java
@@ -1,58 +1,105 @@
 package com.therdl.client.view.widget;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.web.bindery.autobean.shared.AutoBean;
 import com.therdl.shared.beans.SnipBean;
 import org.gwtbootstrap3.client.ui.Modal;
 import org.gwtbootstrap3.client.ui.ModalBody;
+import org.gwtbootstrap3.client.ui.RadioButton;
 
 /**
  * Popup shown to the user to become an RDL supporter
  */
 public class SupportRdlPopup extends Composite {
 
-	interface SupportRdlPopupUiBinder extends UiBinder<Widget, SupportRdlPopup> {
-	}
+    interface SupportRdlPopupUiBinder extends UiBinder<Widget, SupportRdlPopup> {
+    }
 
-	private static SupportRdlPopupUiBinder ourUiBinder = GWT.create(SupportRdlPopupUiBinder.class);
+    private static SupportRdlPopupUiBinder ourUiBinder = GWT.create(SupportRdlPopupUiBinder.class);
 
-	@UiField
-	Modal rdlSupporterModal;
-	@UiField
-	ModalBody modalBody;
+    @UiField
+    Modal rdlSupporterModal;
+    @UiField
+    ModalBody modalBody;
 
-	private boolean populated = false;
+    @UiField
+    RadioButton radButUsd;
 
-	public SupportRdlPopup() {
-		initWidget(ourUiBinder.createAndBindUi(this));
-	}
+    @UiField
+    Anchor ancSupporter;
+    @UiField
+    RadioButton radButGbp;
+    @UiField
+    RadioButton radButEur;
 
-	public void show() {
-		rdlSupporterModal.show();
-	}
+    private boolean populated = false;
 
-	public void hide() {
-		rdlSupporterModal.hide();
-	}
+    public SupportRdlPopup() {
+        initWidget(ourUiBinder.createAndBindUi(this));
 
-	public void populateBody(AutoBean<SnipBean> contentBean) {
-		if (!populated) {
-			modalBody.clear();
-			modalBody.getElement().setInnerHTML(contentBean.as().getContent());
-		}
-		populated = true;
-	}
+        radButUsd.setActive(true);
+    }
 
-	public boolean isPopulated() {
-		return populated;
-	}
+    public void show() {
+        rdlSupporterModal.show();
+    }
 
-	public void setTitle(String title) {
-		rdlSupporterModal.setTitle(title);
-	}
+    public void hide() {
+        rdlSupporterModal.hide();
+    }
+
+    public void populateBody(AutoBean<SnipBean> contentBean) {
+        if (!populated) {
+            modalBody.clear();
+            modalBody.getElement().setInnerHTML(contentBean.as().getContent());
+        }
+        populated = true;
+    }
+
+    public boolean isPopulated() {
+        return populated;
+    }
+
+    public void setTitle(String title) {
+        rdlSupporterModal.setTitle(title);
+    }
+
+    @UiHandler("radButUsd")
+    void handleRadButUsd(ClickEvent e) {
+        radButUsd.setActive(true);
+        radButGbp.setActive(false);
+        radButEur.setActive(false);
+
+        ancSupporter.setHref("/rdl/paypalCheckout?cur=USD");
+    }
+
+    @UiHandler("radButGbp")
+    void handleRadButGbp(ClickEvent e) {
+        radButUsd.setActive(false);
+        radButGbp.setActive(true);
+        radButEur.setActive(false);
+        ancSupporter.setHref("/rdl/paypalCheckout?cur=GBP");
+    }
+
+    @UiHandler("radButEur")
+    void handleRadButEur(ClickEvent e) {
+        radButUsd.setActive(false);
+        radButGbp.setActive(false);
+        radButEur.setActive(true);
+        ancSupporter.setHref("/rdl/paypalCheckout?cur=EUR");
+    }
+
+//    @UiHandler("radButUsd")
+//    void handleRadButUsd(ClickEvent e) {
+//
+//        ancSupporter.setHref("href=/rdl/paypalCheckout?cur=USD");
+//    }
 
 }
